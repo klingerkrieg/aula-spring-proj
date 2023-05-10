90c64e3579281ab83e9f3406e5947706f8a74008
klingerkrieg <klingerkrieg@gmail.com>
2023-05-10 16:08:36 -0300

    CORS

--- src/main/java/com/example/demo/person/PersonRepository.java
+++ src/main/java/com/example/demo/person/PersonRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@CrossOrigin(origins = "*")
 public interface PersonRepository extends 
             PagingAndSortingRepository<Person, Long>, 
             CrudRepository<Person,Long> {
--- src/main/java/com/example/demo/projects/ProjectRepository.java
+++ src/main/java/com/example/demo/projects/ProjectRepository.java
@@ -2,8 +2,10 @@ package com.example.demo.projects;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "*")
 public interface ProjectRepository extends CrudRepository<Project,Long> {
 
 }
